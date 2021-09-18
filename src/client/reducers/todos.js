fb0c5f14a7347fd27b93ed82653ec8267284b5b0
Mike Wittenauer <mikewittenauer@me.com>
2021-09-18 09:53:45 -0400

    Add delete capabality

--- src/client/reducers/todos.js
+++ src/client/reducers/todos.js
@@ -1,10 +1,12 @@
-import { ADD_TODO } from '../actions/actionTypes';
+import { ADD_TODO, DELETE_TODO } from '../actions/actionTypes';
 const initialState = [{ title: 'title', description: 'description' }];
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case ADD_TODO:
             return [ action.payload, ...state ];
+        case DELETE_TODO:
+            return state.filter( (item, index) => index !== action.payload);
         default:
             return state;
     }
